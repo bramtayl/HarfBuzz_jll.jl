cba1cc27a94cc9a3aea0ce37896be631db86d6aa
Elliot Saba <staticfloat@gmail.com>
2019-09-02 23:19:24 -0700

    HarfBuzz_jll build 2.6.1+0

---
+++ src/HarfBuzz_jll.jl
@@ -0,0 +1,26 @@
+module HarfBuzz_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("2e76f6c2-a576-52d4-95c1-20adfe4de566"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "HarfBuzz", artifacts_toml) for e in artifacts["HarfBuzz"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load HarfBuzz; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module HarfBuzz_jll
