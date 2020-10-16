98112c9c64877bc42b4ce20c924e74accbcaeab8
jlbuild <juliabuildbot@gmail.com>
2020-10-16 18:48:29 +0000

    HarfBuzz_jll build 2.6.1+10

--- src/wrappers/i686-linux-gnu.jl
+++ src/wrappers/i686-linux-gnu.jl
@@ -9,118 +9,36 @@ using Gettext_jll
 using Fontconfig_jll
 using Cairo_jll
 using ICU_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libharfbuzz`
-const libharfbuzz_splitpath = ["lib", "libharfbuzz.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libharfbuzz_path = ""
-
-# libharfbuzz-specific global declaration
-# This will be filled out by __init__()
-libharfbuzz_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libharfbuzz = "libharfbuzz.so.0"
-
-
-# Relative path to `libharfbuzz_gobject`
-const libharfbuzz_gobject_splitpath = ["lib", "libharfbuzz-gobject.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libharfbuzz_gobject_path = ""
-
-# libharfbuzz_gobject-specific global declaration
-# This will be filled out by __init__()
-libharfbuzz_gobject_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libharfbuzz_gobject = "libharfbuzz-gobject.so.0"
-
-
-# Relative path to `libharfbuzz_icu`
-const libharfbuzz_icu_splitpath = ["lib", "libharfbuzz-icu.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libharfbuzz_icu_path = ""
-
-# libharfbuzz_icu-specific global declaration
-# This will be filled out by __init__()
-libharfbuzz_icu_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libharfbuzz_icu = "libharfbuzz-icu.so.0"
-
-
-# Relative path to `libharfbuzz_subset`
-const libharfbuzz_subset_splitpath = ["lib", "libharfbuzz-subset.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libharfbuzz_subset_path = ""
-
-# libharfbuzz_subset-specific global declaration
-# This will be filled out by __init__()
-libharfbuzz_subset_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libharfbuzz_subset = "libharfbuzz-subset.so.0"
-
-
-# Inform that the wrapper is available for this platform
-wrapper_available = true
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("HarfBuzz")
+JLLWrappers.@declare_library_product(libharfbuzz, "libharfbuzz.so.0")
+JLLWrappers.@declare_library_product(libharfbuzz_gobject, "libharfbuzz-gobject.so.0")
+JLLWrappers.@declare_library_product(libharfbuzz_icu, "libharfbuzz-icu.so.0")
+JLLWrappers.@declare_library_product(libharfbuzz_subset, "libharfbuzz-subset.so.0")
 function __init__()
-    # This either calls `@artifact_str()`, or returns a constant string if we're overridden.
-    global artifact_dir = find_artifact_dir()
+    JLLWrappers.@generate_init_header(Glib_jll, FreeType2_jll, Graphite2_jll, Libffi_jll, Gettext_jll, Fontconfig_jll, Cairo_jll, ICU_jll)
+    JLLWrappers.@init_library_product(
+        libharfbuzz,
+        "lib/libharfbuzz.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global PATH_list, LIBPATH_list
-    # Initialize PATH and LIBPATH environment variable listings
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Glib_jll.PATH_list, FreeType2_jll.PATH_list, Graphite2_jll.PATH_list, Libffi_jll.PATH_list, Gettext_jll.PATH_list, Fontconfig_jll.PATH_list, Cairo_jll.PATH_list, ICU_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Glib_jll.LIBPATH_list, FreeType2_jll.LIBPATH_list, Graphite2_jll.LIBPATH_list, Libffi_jll.LIBPATH_list, Gettext_jll.LIBPATH_list, Fontconfig_jll.LIBPATH_list, Cairo_jll.LIBPATH_list, ICU_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libharfbuzz_gobject,
+        "lib/libharfbuzz-gobject.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libharfbuzz_path = normpath(joinpath(artifact_dir, libharfbuzz_splitpath...))
+    JLLWrappers.@init_library_product(
+        libharfbuzz_icu,
+        "lib/libharfbuzz-icu.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libharfbuzz_handle = dlopen(libharfbuzz_path, RTLD_LAZY | RTLD_DEEPBIND)
-    push!(LIBPATH_list, dirname(libharfbuzz_path))
+    JLLWrappers.@init_library_product(
+        libharfbuzz_subset,
+        "lib/libharfbuzz-subset.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libharfbuzz_gobject_path = normpath(joinpath(artifact_dir, libharfbuzz_gobject_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libharfbuzz_gobject_handle = dlopen(libharfbuzz_gobject_path, RTLD_LAZY | RTLD_DEEPBIND)
-    push!(LIBPATH_list, dirname(libharfbuzz_gobject_path))
-
-    global libharfbuzz_icu_path = normpath(joinpath(artifact_dir, libharfbuzz_icu_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libharfbuzz_icu_handle = dlopen(libharfbuzz_icu_path, RTLD_LAZY | RTLD_DEEPBIND)
-    push!(LIBPATH_list, dirname(libharfbuzz_icu_path))
-
-    global libharfbuzz_subset_path = normpath(joinpath(artifact_dir, libharfbuzz_subset_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libharfbuzz_subset_handle = dlopen(libharfbuzz_subset_path, RTLD_LAZY | RTLD_DEEPBIND)
-    push!(LIBPATH_list, dirname(libharfbuzz_subset_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
